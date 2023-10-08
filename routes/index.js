1c24e04509769bf3dd44708aec21c6f756d13484
kamothi <rhkddlf7911@naver.com>
2023-10-08 16:18:06 +0900

    Feature: add communityRouter and dataUserRouter

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,12 @@ const router = express.Router();
 
 // const userRouter = require("./userRouter");
 const publicDataRouter = require("../publicData/publicDataRouter");
-
+const communityRouter = require('../models/community/controller/index');
+const dataUseRouter = require('../models/dataUse/controller/index');
 // router.use("/users", userRouter.router);
 router.use("/resources", publicDataRouter.router);
+router.use(communityRouter.path,communityRouter.router);
+router.use(dataUseRouter.path,dataUseRouter.router);
+
 
 module.exports = router;
