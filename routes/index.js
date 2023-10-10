54c52cec22ad6ce5e0c978d7aef7eecadf316ed7
seonwook0215 <58116065+seonwook0215@users.noreply.github.com>
2023-10-10 16:14:13 +0900

    Feature: add dataRequest & user routes

--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,15 @@ const router = express.Router();
 // const userRouter = require("./userRouter");
 const publicDataRouter = require("../publicData/publicDataRouter");
 const communityRouter = require('../models/community/controller/index');
+const dataRequestRouter = require('../models/dataRequest/controller/index');
 const dataUseRouter = require('../models/dataUse/controller/index');
+const userRouter = require('../models/user/controller/index');
+
 // router.use("/users", userRouter.router);
 router.use("/resources", publicDataRouter.router);
 router.use(communityRouter.path,communityRouter.router);
+router.use(dataRequestRouter.path,dataRequestRouter.router);
 router.use(dataUseRouter.path,dataUseRouter.router);
-
+router.use(userRouter.path,userRouter.router);
 
 module.exports = router;
